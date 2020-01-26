84011db5043fd4bc8aef3101ba3ebc88ffd7304a
Joao Franco <joao.franco@focus-bc.com>
2020-01-26 18:41:44 +0000

    Storage must be an abstract class bc interfaces are not supported in ioc

--- src/storage/Storage.ts
+++ src/storage/Storage.ts
@@ -1,10 +1,11 @@
 import { Disposable } from '@jsfsi-core/typescript-cross-platform'
 
-export interface Storage<K, V> extends Disposable {
-    set(key: K, value: V): Promise<void>
-    set(key: K, value: V): Promise<void>
-    get(key: K): Promise<V>
-    expireIn(key: K, seconds: number): Promise<void>
-    delete(key: K): Promise<void>
-    clear(): Promise<void>
+export abstract class Storage<K, V> implements Disposable {
+    abstract set(key: K, value: V): Promise<void>
+    abstract set(key: K, value: V): Promise<void>
+    abstract get(key: K): Promise<V>
+    abstract expireIn(key: K, seconds: number): Promise<void>
+    abstract delete(key: K): Promise<void>
+    abstract clear(): Promise<void>
+    abstract dispose(): Promise<void>
 }
