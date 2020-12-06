06a729e7a09e1c6f4e7af3018f5f65a4fb4fd1c3
Joao Franco <joao.franco@jsfsi.com>
2020-12-06 21:47:10 +0000

    Add missing file

---
+++ src/Guid.ts
@@ -0,0 +1,25 @@
+import uuid from 'uuid'
+
+export class Guid {
+    private _uuid: string
+
+    private constructor(value: string) {
+        this._uuid = value
+    }
+
+    public toString() {
+        return this._uuid
+    }
+
+    public equals(other: Guid) {
+        return this._uuid === other.toString()
+    }
+
+    public static New() {
+        return new Guid(uuid.v4())
+    }
+
+    public static Empty() {
+        return new Guid(uuid.NIL)
+    }
+}
