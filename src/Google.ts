15a5d6c03d58483936baae7967c2914279b3e22d
Joao Franco <joao.franco@jsfsi.com>
2021-01-09 12:53:26 +0000

    Change google api that provides user name

--- src/Google.ts
+++ src/Google.ts
@@ -1,16 +1,19 @@
 import { HttpRequest, HttpMethods } from '@jsfsi-core/typescript-cross-platform'
+import { executeShell } from './Shell'
 
 export interface GoogleUser {
-    azp?: string
-    aud?: string
-    sub?: string
+    id?: string
     email?: string
-    name?: string
+    given_name?: string
+    family_name?: string
     picture?: string
+    locale?: string
 }
 
 export class Google {
-    constructor(private tokenInfoURL: string) {}
+    constructor(
+        private tokenInfoURL = 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=',
+    ) {}
 
     async userInfo(accessToken: string) {
         const request = await HttpRequest.fetch<GoogleUser>({
@@ -19,5 +22,13 @@ export class Google {
         })
 
         return request?.data
+    }
+
+    async getToken() {
+        let googleAccessToken = (await executeShell(
+            'gcloud auth print-access-token',
+        )) as string
+        googleAccessToken = googleAccessToken.substring(0, googleAccessToken.length - 1)
+        return googleAccessToken
     }
 }
---
+++ tests/integration/google.spec.ts
@@ -0,0 +1,17 @@
+import { Google } from '../../src/Google'
+
+describe('Google user info', () => {
+    const google = new Google()
+
+    let googleAccessToken: string
+    beforeAll(async () => {
+        googleAccessToken = await google.getToken()
+    })
+
+    it('Get user info with token', async () => {
+        const userInfo = await google.userInfo(googleAccessToken)
+        expect(userInfo.id).toBeDefined()
+        expect(userInfo.email).toBeDefined()
+        expect(userInfo.picture).toBeDefined()
+    })
+})
